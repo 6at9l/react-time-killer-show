82e1c2f18eaab9aa96be6ee5be730034bb08fafd
6at9l <6at9lodi@gmail.com>
2020-03-16 01:03:35 +0200

    Add files via upload

---
+++ precache-manifest.8729bf2a71e78bbf4c4c78e79195e0c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8e07d74c6080c7b0c68cde784e4fde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44071c2284e74de0b3b1",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bf1ae79aba3614ca6d32",
+    "url": "/static/js/2.0c648e34.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.0c648e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44071c2284e74de0b3b1",
+    "url": "/static/js/main.6a193430.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "4359c6bb27653490d1ac2ba963fa82a6",
+    "url": "/static/media/background.4359c6bb.png"
+  },
+  {
+    "revision": "b60e4bc1b157d59ea20bd484c25499cc",
+    "url": "/static/media/lightShot.b60e4bc1.png"
+  },
+  {
+    "revision": "06b0e7ddd105e11dc08415aaf77a4e2e",
+    "url": "/static/media/monster.06b0e7dd.png"
+  },
+  {
+    "revision": "516ee8f1d41553d80795879457c5763f",
+    "url": "/static/media/wizard.516ee8f1.png"
+  }
+]);
